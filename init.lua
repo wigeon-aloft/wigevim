481d0ade3e617052e0789a7130c800a37dc8fcf8
wigeon-aloft <willem@thevanhoofs.com>
2026-05-03 15:31:50 +0100

    style: fixed inconsistent leading spaces on some lines

--- init.lua
+++ init.lua
@@ -1,17 +1,17 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
+        { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+        { out, "WarningMsg" },
+        { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
     os.exit(1)
-  end
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -60,7 +60,7 @@ require("config.keybinds")
 
 -- Setup lazy.nvim
 require("lazy").setup({
-  spec = {
+    spec = {
     {'tknightz/telescope-termfinder.nvim'},
 
     require("plugins.alpha"),
@@ -72,6 +72,6 @@ require("lazy").setup({
     require("plugins.toggleterm"),
     require("plugins.which-key"),
   },
-  install = { colorscheme = { "gruvbox-material" } },
-  checker = { enabled = true },
+    install = { colorscheme = { "gruvbox-material" } },
+    checker = { enabled = true },
 })
