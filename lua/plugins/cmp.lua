9990f9e1d3c39702ef3ca06c3aa19f7d1c36db0e
wigeon-aloft <willem@thevanhoofs.com>
2026-05-03 15:51:19 +0100

    feat: updated cmp keybindings and added path source

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -19,7 +19,8 @@ return {
               ['<C-f>'] = cmp.mapping.scroll_docs(4),
               ['<C-Space>'] = cmp.mapping.complete(),
               ['<C-e>'] = cmp.mapping.abort(),
-              ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+              ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+              ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
             }),
             window = {
                 completion = cmp.config.window.bordered(),
@@ -27,6 +28,7 @@ return {
             },
             sources = {
                 { name = "nvim_lsp" },
+                { name = "async_path"},
             },
         })
     end,
