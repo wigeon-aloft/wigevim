8dacfc04fe9a5b1acdcb6ab06506c8d575c06228
wigeon-aloft <willem@thevanhoofs.com>
2026-05-09 23:56:34 +0100

    fix: updated alpha plugin to use devicons

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,7 +1,11 @@
 return {
     'goolord/alpha-nvim',
-    dependencies = { 'nvim-mini/mini.icons' },
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function ()
-        require'alpha'.setup(require'alpha.themes.startify'.config)
+        local startify = require("alpha.themes.startify")
+        startify.file_icons.provider = "devicons"
+        require("alpha").setup(
+            startify.config
+        )
     end
 }
